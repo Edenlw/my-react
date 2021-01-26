df103e2b37856c7535c38f90f86b086a70f786a8
Edenlw <568885816@qq.com>
2021-01-26 15:51:47 +0800

    1

--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -30,7 +30,7 @@ export default class App extends React.Component {
   }
   render() {
     return (
-      <div className={styles.header}>
+        <div className={styles.header}>
         {this.state.list.map((item) => {
           return (
             <Link
